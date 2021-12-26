56084bcd0fc1c859a95acef16195daa38f70f34c
Juan Leni <lenijuan@gmail.com>
2021-12-26 01:00:57 +0100

    enable rust environment

--- Makefile
+++ Makefile
@@ -11,11 +11,12 @@ git-update:
 ccache-setup:
 	@cd buildroot && make CCACHE_OPTIONS="--max-size=50G --zero-stats" ccache-options
 
+# To exit QEMU use ctrl-a + X
 start-qemu-host:
 	@cd $(IMAGES) && ../host/bin/qemu-system-arm \
 	-machine virt -machine secure=on -cpu cortex-a15 \
 	-smp 1 -s -m 1024 -d unimp \
-	-serial stdio \
+	-serial mon:stdio \
 	-netdev user,id=vmnic -device virtio-net-device,netdev=vmnic \
 	-semihosting-config enable,target=native \
 	-bios flash.bin
--- package/zondax/hellorustee/hellorustee.mk
+++ package/zondax/hellorustee/hellorustee.mk
@@ -1,30 +1,35 @@
-# define variables here
-# HELLORUSTEE_VERSION=master
-# HELLORUSTEE_SITE_METHOD=git
-# HELLORUSTEE_SITE=https://github.com/Zondax/hello-rustee
-# HELLORUSTEE_LICENSE=Apache-2.0
-# LINUX_LICENSE_FILES=LICENSE
+# reference: https://buildroot.org/downloads/manual/manual.html#_integration_of_cargo_based_packages
+# to simplify the development workflow, we use a submodule
 
 HELLORUSTEE_SITE=$(BR2_EXTERNAL_ZONDAXTEE_PATH)/external/hello-rustee
 HELLORUSTEE_SITE_METHOD=local
 HELLORUSTEE_DEPENDENCIES=
 
-ifeq ($(BR2_PACKAGE_HELLOZONDAX_SOMETHING),y)
+# get correct rust enviroment
+HELLORUSTEE_DEPENDENCIES = host-rustc
+HELLORUSTEE_CARGO_ENV = CARGO_HOME=$(HOST_DIR)/share/cargo
+HELLORUSTEE_BIN_DIR = target/$(RUSTC_TARGET_NAME)/$(HELLORUSTEE_CARGO_MODE)
+HELLORUSTEE_CARGO_OPTS = \
+     $(if $(BR2_ENABLE_DEBUG),,--release) \
+     --target=$(RUSTC_TARGET_NAME) \
+     --manifest-path=$(@D)/Cargo.toml
+
+ifeq ($(BR2_PACKAGE_HELLORUSTEE_SOMEOPTION),y)
 # Add some more settings here
 #HELLORUSTEE_DEPENDENCIES += readline
 endif
 
-# Start at bootime
-# Slide 208
-
 #	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(TARGET_CFLAGS)
 define HELLORUSTEE_BUILD_CMDS
-	$(MAKE) -C $(@D)
+	$(TARGET_MAKE_ENV) $(HELLORUSTEE_CARGO_ENV) cargo build $(HELLORUSTEE_CARGO_OPTS)
 endef
 
 define HELLORUSTEE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(HELLORUSTEE_DIR)/hello-rustee $(TARGET_DIR)/usr/bin/hello-rustee
 endef
+
+# TODO: Start at bootime
+# Slide 208
 
 # Use generic package infrastructure
 $(eval $(generic-package))
