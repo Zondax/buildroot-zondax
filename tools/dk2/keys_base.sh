6421a143eef001bc76c56a23e9add8204ad30ab8
Natanael <neithanmo@gmail.com>
2022-01-03 08:30:00 -0600

    feat(secure_boot) Split the scripts to generate keys and sign images

---
+++ tools/dk2/keys_base.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Path to images directory
+script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+buildroot_path=$script_path/../../
+ls $buildroot_path
+imageDir=$buildroot_path/images/
+keysDir=$buildroot_path/keys/
+# Keys for signing
+privName="privateKey"
+pubName="publicKey"
+pubKeyHashName="publicKeyhash"
+privateKey=${privName}.pem
+publicKey=${pubName}.pem
+pkh=${pubKeyHashName}.bin
+
+# Dummy password for keygen tools to
+pass="test"
+
+# Image names
+plainImageName=tf-a-stm32mp157c-dk2-mx.stm32
+signedImageName=tf-a-stm32mp157c-dk2-mx_Signed.stm32
