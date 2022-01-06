48660f8082b5c2499ae3bbf23f1f19a961eb0266
Natanael <neithanmo@gmail.com>
2022-01-05 22:17:30 -0600

    feat(tee-service) Add the tee-substrate-service package

---
+++ package/zondax/tee-service/README.md
@@ -0,0 +1,6 @@
+# Notes
+
+Reference: https://bootlin.com/doc/training/buildroot/buildroot-slides.pdf
+
+Slide 136. generic-package is for non-standard build systems
+
---
+++ package/zondax/tee-service/tee-service.mk
@@ -0,0 +1,62 @@
+# reference: https://buildroot.org/downloads/manual/manual.html#_integration_of_cargo_based_packages
+# to simplify the development workflow, we use a submodule
+
+TEE_SERVICE_SITE=$(BR2_EXTERNAL_ZONDAXTEE_PATH)/external/tee-service
+TEE_SERVICE_SITE_METHOD=local
+
+# get correct rust enviroment
+TEE_SERVICE_DEPENDENCIES = host-python host-rustc-nightly optee-client optee-os
+TEE_SERVICE_BIN_DIR = target/$(RUSTC_TARGET_NAME)/$(TEE_SERVICE_CARGO_MODE)
+TEE_SERVICE_CARGO_OPTS = \
+     $(if $(BR2_ENABLE_DEBUG),,--release) \
+     --target=$(RUSTC_TARGET_NAME)
+
+TEE_SERVICE_TA=TEE
+TEE_SERVICE_HOST=REE
+TEE_SERVICE_FRAMEWORK_TA_INCLUDE=$(TEE_SERVICE_DIR)/framework/ta/src/include
+
+TEE_SERVICE_CFLAGS 	= -Wall -I$(TEE_SERVICE_DIR)/$(TEE_SERVICE_TA)/lib/include -I$(TEE_SERVICE_FRAMEWORK_TA_INCLUDE) -I$(STAGING_DIR)/usr/include -I./include -fPIC
+TEE_SERVICE_CFLAGS 	+= -I$(TEE_SERVICE_DIR)/$(TEE_SERVICE_HOST)/lib/include
+
+
+# ENV Variables needed to build our application it tells make and rust compiler where to find internal headers
+# and dependencies
+TEE_SERVICE_ENV = CARGO_HOME=$(HOST_DIR)/share/cargo RUST_TARGET=$(RUSTC_TARGET_NAME) SRC_=$(TEE_SERVICE_DIR) OVERRIDE_SYSROOT=1 \
+                        TEEC_EXPORT=$(STAGING_DIR) CROSS_COMPILE=$(TARGET_CROSS) TA_DEV_KIT_DIR=$(OPTEE_OS_SDK)  \
+						TA_CROSS_COMPILE=$(TARGET_CROSS)
+TEE_SERVICE_SYSTEMD_DIR=${TARGET_DIR}/usr/lib/systemd/system/
+
+TEE_SERVICE_CONFIGURE_OPTS += CFLAGS="$(TARGET_CFLAGS) $(TEE_SERVICE_CFLAGS)"
+
+ifeq ($(BR2_PACKAGE_TEE_SERVICE_SOMEOPTION),y)
+# Add some more settings here
+#TEE_SERVICE_DEPENDENCIES += readline
+endif
+
+#	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(TARGET_CFLAGS)
+define TEE_SERVICE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TEE_SERVICE_ENV) $(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) $(TEE_SERVICE_CONFIGURE_OPTS)
+endef
+
+define TEE_SERVICE_CONFIGURE_CMDS
+    $(PYTHON) $(TEE_SERVICE_DIR)/framework/ta/src/newuuid.py
+endef
+
+define TEE_SERVICE_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(TEE_SERVICE_DIR)/framework/host/src/rustee_app $(TARGET_DIR)/sbin/tee-substrate-service
+    $(INSTALL) -D -m 0444 $(TEE_SERVICE_DIR)/framework/ta/src/*.ta $(TARGET_DIR)/lib/optee_armtz
+endef
+
+define TEE_SERVICE_INSTALL_INIT_SYSTEMD
+	sed -i -e s:@sysconfdir@:$(TEE_SERVICE_SYSTEMD_DIR):g \
+		-e s:@sbindir@:$(TARGET_DIR)/sbin:g \
+		$(TEE_SERVICE_DIR)/tee-substrate-service.service
+
+	$(INSTALL) -D -m 644 $(TEE_SERVICE_DIR)/tee-substrate-service.service $(TEE_SERVICE_SYSTEMD_DIR)/tee-substrate-service.service
+endef
+
+# TODO: Start at bootime
+# Slide 208
+
+# Use generic package infrastructure
+$(eval $(generic-package))
