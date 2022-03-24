406907a2dd7803dfa55c3d069f39a76a5604bc7b
Thomas Perrot <thomas.perrot@bootlin.com>
2022-03-24 11:39:44 +0100

    fix stm32m157f boot
    Signed-off-by: Thomas Perrot <thomas.perrot@bootlin.com>

--- tools/dk2/sign_image.sh
+++ tools/dk2/sign_image.sh
@@ -7,13 +7,12 @@ source $script_path/keys_base.sh
 
 ################################
 # 1. Sign with STM32MP_SigningTool_CLI  images/arm-trusted-firmware  BL2 variant (sdcard, *.stm32 file)
-# 2. The post_image script will detecd there is a signed file and use it 
+# 2. The post_image script will detecd there is a signed file and use it
 
 #Sign with STM32MP_SigningTool_CLI
 STM32MP_SigningTool_CLI -prvk $keysDir/$privateKey -pubk $keysDir/$publicKey -pwd $pass \
     -bin $imageDir/$plainImageName -iv 5 -of 0 \
-    -t fsbl \
-    -la 0x2ffc2500 -ep 0x2ffe4002
+    -t fsbl
 if [ $? -eq 0 ]; then
     echo "*.stm32 signed successful"
 else
