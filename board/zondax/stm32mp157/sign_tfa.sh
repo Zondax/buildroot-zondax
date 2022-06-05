0612d91cb7acc434dc6efbef61d2c7aafa6a03f3
Natanael <neithanmo@gmail.com>
2022-06-05 16:29:30 -0600

    add script to sign TFA

---
+++ board/zondax/stm32mp157/sign_tfa.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+images_dir=$BR2_EXTERNAL_ZONDAXTEE_PATH/images/
+tools_dir=$BR2_EXTERNAL_ZONDAXTEE_PATH/tools/dk2/
+
+tf_a_suffix="stm32"
+
+source $tools_dir/keys_base.sh
+
+for img_file in "${BINARIES_DIR}/"*".${tf_a_suffix}"; do
+    [ -e "$img_file" ] || continue
+    python3 $tools_dir/sign_tfa.py "$img_file" \
+        "${keysDir}/${privateKey}" "${pass}"
+    if [ $? -eq 0 ]; then
+        echo "$img_file signed"
+    else
+        echo "Signing $img_file failed"
+        exit 1
+    fi
+done
+
+
